c49a79682efa1b6171cb39d7c83ca0706b1510cc
21xhr <116286892+21xhr@users.noreply.github.com>
2026-03-19 21:30:31 +0000

    chore(backup): Daily Supabase DB Backup

--- supabase/backups/data.sql
+++ supabase/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict DUPpO17nJUINj661wFzK9meuGALdrZRhTnwVDzL3iveNAGFqDH0Bit5BHZJiTTj
+-- \restrict KtD9JZHwcVbQH4SCPutVh4Mb77gCd8ywb2pbn6nsVKJbNXTXhADfaRMLwjagbvo
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -716,6 +716,6 @@ SELECT pg_catalog.setval('"public"."users_user_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict DUPpO17nJUINj661wFzK9meuGALdrZRhTnwVDzL3iveNAGFqDH0Bit5BHZJiTTj
+-- \unrestrict KtD9JZHwcVbQH4SCPutVh4Mb77gCd8ywb2pbn6nsVKJbNXTXhADfaRMLwjagbvo
 
 RESET ALL;
